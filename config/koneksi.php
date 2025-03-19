9a89c909338895826e755f6968b01cb1ab4b47d7
YuannZing <dirgafarrel2008@gmail.com>
2025-03-19 08:59:09 +0700

    change pdo

--- config/koneksi.php
+++ config/koneksi.php
@@ -4,8 +4,13 @@ $dbname = 'tracer_study';
 $username = 'root';
 $password = '';
 
-$conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+// $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$conn = new mysqli($host, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
 
 date_default_timezone_set('Asia/Jakarta');
 // s
