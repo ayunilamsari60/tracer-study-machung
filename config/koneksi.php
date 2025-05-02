3f911ff6ed11d145a97646603991b66ba8e614f6
YuannZing <dirgafarrel2008@gmail.com>
2025-05-02 20:04:01 +0700

    add connection

--- config/koneksi.php
+++ config/koneksi.php
@@ -19,6 +19,16 @@ try {
     error_log($e->getMessage());
 }
 
+$host2 = '127.0.0.1';
+$dbname2 = 'akademik';
+$username2 = 'root';
+$password2 = '';
+
+$conn2 = new mysqli($host2, $username2, $password2, $dbname2);
+if ($conn2->connect_error) {
+    die("Koneksi gagal: " . $conn2->connect_error);
+}
+
 
 date_default_timezone_set('Asia/Jakarta');
 ?>
